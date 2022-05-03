01dbba76910628cdb993d45d389de898b4543848
arauzk <arauzkm@gmail.com>
2022-05-03 11:40:36 -0500

    Agregue el codigo inicial de la clase 2

---
+++ Clase2.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(kableExtra)
+library(knitr)
+
+#Pasos para un repositorio
+#Git add agregar archivos que vas a guardar
+#Git commit guardar en el repositorio local (mi pc)
+#Git push guargar en la nube
